cb7678cb8a7c3d4a8e4ec841a0ed3faa3c430af8
Xudong (Don) Wang <xudon9@outlook.com>
2023-05-29 13:20:17 +1000

    Leetcode 1603

---
+++ leetcode/1603.design-parking-system/solution.cpp
@@ -0,0 +1,24 @@
+class ParkingSystem {
+    uint16_t arr[3];
+
+public:
+    ParkingSystem(int big, int medium, int small) {
+        arr[0] = big;
+        arr[1] = medium;
+        arr[2] = small;
+    }
+
+    bool addCar(int carType) {
+        if (arr[carType-1]) {
+            --arr[carType-1];
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
